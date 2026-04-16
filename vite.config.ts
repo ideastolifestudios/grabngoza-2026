13520a5ed259b1b7b12d6568683ee0b249715339
ideastolifestudios <cbrprints22@gmail.com>
2026-04-16 19:17:03 +0200

    move firebase config to src, fix import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@config': path.resolve(__dirname, 'firebase-applet-config.json'),
+    },
+  },
+  base: '/',
 });
