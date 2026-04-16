84aa7aebed3968b2c3d4c60c492f7a27a1a15486
ideastolifestudios <cbrprints22@gmail.com>
2026-04-16 19:31:47 +0200

    use web config, remove exposed service account key

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcQWjvjcwO5S-KQoTP-6sRIOJkHCKBj1I",
+  authDomain: "grab-go-za.firebaseapp.com",
+  projectId: "grab-go-za",
+  storageBucket: "grab-go-za.firebasestorage.app",
+  messagingSenderId: "111606509611",
+  appId: "1:111606509611:web:10dcbcdb2e39a052b47a6e",
+  measurementId: "G-L979BBPS1K"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-export const db = firebaseConfig.databaseId 
-  ? getFirestore(app, firebaseConfig.databaseId)
-  : getFirestore(app);
-
+export const db = getFirestore(app, 'grab-go-za');
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
-
 export default app;
